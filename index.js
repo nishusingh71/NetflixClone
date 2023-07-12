405b71cd0c6c03207ab77326891d4a4e5e482bb7
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2023-07-12 17:29:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const imgPath = "https://image.tmdb.org/t/p/original";
 const apiPaths={
     fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
     fetchMoviesList: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
-    fetchTrending:`${apiEndpoint}/trending/all/week?api_key=${apikey}&language=en-US`,
+    fetchTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
     searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC0SZJkHFX-fQ7NrsxdI4l4mGwYuY4l7P8`
 }
 //Boots up the app
